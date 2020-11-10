b25bea081c7a05d967095d43a84d3d4dec74f277
laith budairi <laith.budairi@exalt.ps>
2020-11-10 12:11:03 +0200

    Intial Commit

---
+++ cmake-build-debug/CMakeFiles/cpp_tutorial.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_tutorial.dir/main.cpp.obj"
+  "cpp_tutorial.exe"
+  "cpp_tutorial.exe.manifest"
+  "cpp_tutorial.pdb"
+  "libcpp_tutorial.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_tutorial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ input/UserInput.h
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+static void userInput() {
+    int x;
+
+    cout << "Type a number: ";
+    cin >> x;
+    cout << "Number is: " << x;
+}
